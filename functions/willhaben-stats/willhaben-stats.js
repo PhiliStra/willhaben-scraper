db15e757aa9e2f7ffe733f24a53b9fc6976b5537
Stefan Kandlbinder <stefan.kandlbinder@gmail.com>
2022-07-17 18:03:47 +0200

    screenshot

--- functions/willhaben-stats/willhaben-stats.js
+++ functions/willhaben-stats/willhaben-stats.js
@@ -37,6 +37,13 @@ exports.handler = async function (req, res) {
     result.description = description;
     result.title = value.substring(value.indexOf(" "));
 
+    const screenshot = await page.screenshot({
+      type: "jpeg",
+      encoding: "base64",
+    }); */
+
+    result.screenshot = screenshot;
+
     // close the browser
     await browser.close();
 

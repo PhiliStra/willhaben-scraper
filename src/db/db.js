13cd5700642f3ad7d63c64fb13cf5870ad11c6de
Stefan Kandlbinder <stefan.kandlbinder@gmail.com>
2022-07-20 16:54:07 +0200

    -

--- src/db/db.js
+++ src/db/db.js
@@ -3,17 +3,15 @@ import { getDatabase } from "firebase/database";
 
 let firebaseConfig;
 
-if (process.env.NODE_ENV !== "production") {
-  firebaseConfig = {
-    apiKey: process.env.VUE_APP_FIREBASE_API_KEY,
-    authDomain: process.env.VUE_APP_FIREBASE_AUTH_DOMAIN,
-    databaseURL: process.env.VUE_APP_FIREBASE_DATABASE_URL,
-    projectId: process.env.VUE_APP_FIREBASE_PROJECT_ID,
-    storageBucket: process.env.VUE_APP_FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.VUE_APP_FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.VUE_APP_FIREBASE_APP_ID,
-  };
-}
+firebaseConfig = {
+  apiKey: process.env.VUE_APP_FIREBASE_API_KEY,
+  authDomain: process.env.VUE_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.VUE_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.VUE_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.VUE_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.VUE_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.VUE_APP_FIREBASE_APP_ID,
+};
 
 /* eslint-disable no-unused-vars */
 const app = initializeApp(firebaseConfig);

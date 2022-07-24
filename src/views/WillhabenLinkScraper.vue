a945c313b105b94898b654edabe9ead581c71703
Stefan Kandlbinder <stefan.kandlbinder@gmail.com>
2022-07-24 16:18:36 +0200

    ...

--- src/views/WillhabenLinkScraper.vue
+++ src/views/WillhabenLinkScraper.vue
@@ -1,24 +1,32 @@
 <template>
-  <article class="prose max-w-none mt-12 mx-auto">
-    <h1>Link Scraper</h1>
-    <p>Enter a valid link, hover over the link and get ready for sUrPrIsE...</p>
-    <p>
-      <ul>
+  <article class="mt-12">
+    <section class="prose max-w-none mx-auto">
+      <h1>Link Scraper</h1>
+      <p>
+        Enter a valid link, hover over the link and get ready for sUrPrIsE...
+      </p>
+    </section>
+    <section class="mt-8">
+      <ul class="space-y-1">
         <!-- render LinkPreviewer component for each demolink -->
-        <li v-for="link in demoLinks" :key="link">
+        <li
+          class="font-bold hover:underline"
+          v-for="link in demoLinks"
+          :key="link"
+        >
           <link-previewer :targetURL="link" />
         </li>
       </ul>
-    </p>
-    
-    <!-- input field to add new links -->
-    <p><input
-      class="p-2 ring ring-blue-600 rounded-lg shadow-md w-2/3 text-center"
-      type="url"
-      @keyup.enter="addLink"
-      required
-      placeholder="enter valid url"
-    /></p>
+
+      <!-- input field to add new links -->
+      <input
+        class="mt-6 p-2 ring ring-blue-600 rounded-lg shadow-md w-2/3 text-center"
+        type="url"
+        @keyup.enter="addLink"
+        required
+        placeholder="enter valid url"
+      />
+    </section>
   </article>
 </template>
 

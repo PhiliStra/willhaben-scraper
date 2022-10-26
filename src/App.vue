98a25e6a164a51da12015f425fc013b8c42ef229
Stefan Kandlbinder <stefan.kandlbinder@gmail.com>
2022-10-26 22:29:18 +0200

    sticky

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,6 @@
 <template>
-  <nav class="nav relative">
-    <div
-      class="container mx-auto px-4 w-full justify-start flex items-center md:hidden h-16 sticky z-10 top-0 bg-gray-50">
+  <nav class="nav sticky z-10 top-0 bg-gray-50">
+    <div class="container mx-auto px-4 w-full justify-start flex items-center md:hidden h-16">
       <router-link to="/" class="flex mr-8 items-center p-2">
         <svg class="stroke-current w-6 aspect-square" xmlns="http://www.w3.org/2000/svg" xml:space="preserve"
           viewBox="0 0 600 600">

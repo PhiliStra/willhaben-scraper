e831e9bef7f7350439a000eedd187cfbdcd08a4e
Stefan Kandlbinder <stefan.kandlbinder@gmail.com>
2022-10-26 22:12:05 +0200

    ...

--- src/App.vue
+++ src/App.vue
@@ -9,7 +9,7 @@
             d="m79.9 148-57-57C45.4 68.5 72 56.8 90.3 59.5L122 27.8c6-6 15.8-6 21.8 0s6 15.8 0 21.8l-32.3 32.3c2 18.3-9.7 44.2-31.6 66.1zm10.4-88.5-9.5 12.8c-1.7 2.3-1.5 5.6.6 7.7l10.1 10c2 2 5.2 2.3 7.5.7l12.5-8.8"
             stroke-linecap="round" stroke-width="2" stroke-linejoin="round" stroke-miterlimit="10" fill="none" />
         </svg>
-        <span class="text-base font-medium">Scraper</span>
+        <span class="text-base font-medium inline-block ml-1">Scraper</span>
       </router-link>
     </div>
     <div class="bg-gray-50 w-full h-16 fixed bottom-0 md:sticky md:top-0 z-10 items-center justify-center flex">
@@ -21,7 +21,7 @@
               d="m79.9 148-57-57C45.4 68.5 72 56.8 90.3 59.5L122 27.8c6-6 15.8-6 21.8 0s6 15.8 0 21.8l-32.3 32.3c2 18.3-9.7 44.2-31.6 66.1zm10.4-88.5-9.5 12.8c-1.7 2.3-1.5 5.6.6 7.7l10.1 10c2 2 5.2 2.3 7.5.7l12.5-8.8"
               stroke-linecap="round" stroke-width="2" stroke-linejoin="round" stroke-miterlimit="10" fill="none" />
           </svg>
-          <span class="text-base font-medium">Scraper</span>
+          <span class="text-base font-medium inline-block ml-1">Scraper</span>
         </router-link>
         <div class="flex space-x-4 font-medium">
           <router-link to="/chart" class="p-2">
